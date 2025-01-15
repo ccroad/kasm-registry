f612e94480bb5992fe30ea9e8531574f852f67f6
Chris Croad <christopher.croad@gmail.com>
2025-01-15 10:49:52 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     name: 'ChrisCs Custom KASM Registry',
     description: 'ChrisCs KASM Workspaces.',
-    icon: '/img/logo.svg',
+    icon: 'https://github.com/simple-icons/simple-icons/blob/develop/icons/answer.svg',
     listUrl: 'https://https://github.com/ccroad/kasm-registry/',
     contactUrl: 'https://github.com/ccroad/kasm-registry/issues',
   },
